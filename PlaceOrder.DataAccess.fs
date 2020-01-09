20319fc2e45841aa279bc44b748ae27ffb8d379a
Frank Wang <FrankWangSydney@Gmail.com>
2020-01-10 08:01:47 +1100

    Added F# SQL Provider

---
+++ PlaceOrder.DataAccess.fs
@@ -0,0 +1,9 @@
+﻿module PlaceOrder
+
+open FSharp.Data.Sql
+
+let [<Literal>] connStr = "Server=(local);Database=DuxCommerce;User Id=DuxAdmin;Password=Password1;"
+type DuxCommerce = SqlDataProvider<ConnectionString = connStr, UseOptionTypes = true>
+//type DuxCommerce = SqlDataProvider<ConnectionString = connStr, UseOptionTypes = true>
+
+let context = DuxCommerce.GetDataContext()
