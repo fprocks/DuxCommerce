f7d415652afef607d57a009cc357a013b363664b
Frank Wang <FrankWangSydney@Gmail.com>
2020-01-10 20:41:40 +1100

    Added database queries

--- PlaceOrder.DataAccess.fs
+++ PlaceOrder.DataAccess.fs
@@ -4,6 +4,19 @@ open FSharp.Data.Sql
 
 let [<Literal>] connStr = "Server=(local);Database=DuxCommerce;User Id=DuxAdmin;Password=Password1;"
 type DuxCommerce = SqlDataProvider<ConnectionString = connStr, UseOptionTypes = true>
-//type DuxCommerce = SqlDataProvider<ConnectionString = connStr, UseOptionTypes = true>
 
 let context = DuxCommerce.GetDataContext()
+
+let insertCustomer = 
+    let product = context.Dbo.Products.Create()
+    product.Name <- "DDD"
+    product.Price <- 45m
+    context.SubmitUpdates
+
+let getProduct (name:string) = 
+    query {
+        for product in context.Dbo.Products do
+        where (product.Name.Equals(name))
+        take 1
+        select product
+    }
