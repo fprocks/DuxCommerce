502c15754ada7ab106c4c98ce32da53ca2750c7d
Frank Wang <FrankWangSydney@gmail.com>
2020-09-05 16:06:30 +1000

    Added use sample

--- PlaceOrder.DataAccess.fs
+++ PlaceOrder.DataAccess.fs
@@ -13,7 +13,17 @@ type Customer = {
     Id : int
     Name : string
 }
-let insert connString (product:Product) (customer:Customer) =
+
+let insert1 connString (product:Product) (customer:Customer) =
+    ( use connection = new SqlConnection(connString)
+      connection.EnsureOpen()
+      ( use trans = connection.BeginTransaction()
+        connection.Insert<Product, int> (product, transaction = trans)
+        connection.Insert<Customer, int> (customer, transaction = trans)
+        trans.Commit() )
+    )
+    
+let insert2 connString (product:Product) (customer:Customer) =
     using(new SqlConnection(connString)) (fun connection ->
         connection.EnsureOpen()
         using(connection.BeginTransaction()) (fun trans ->
