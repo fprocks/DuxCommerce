564ada557f4c62b91834bf9acad01cf3624392a2
Frank Wang <FrankWangSydney@gmail.com>
2020-10-11 23:12:24 +1100

    Hard coded connection string for now

--- DuxCommerce.Specifications.UseCases/Hooks/DatabaseCleaner.cs
+++ DuxCommerce.Specifications.UseCases/Hooks/DatabaseCleaner.cs
@@ -1,5 +1,6 @@
 ﻿using Respawn;
 using System.Configuration;
+using System.Data.SqlClient;
 
 namespace DuxCommerce.Specifications.UseCases.Hooks
 {
@@ -7,7 +8,11 @@ namespace DuxCommerce.Specifications.UseCases.Hooks
     {
         public static void CleanUp()
         {
-            var clientConnString = ConfigurationManager.ConnectionStrings["EmpLive.WebApi.Client"].ConnectionString;
+            var connString = "data source = (local); initial catalog = DuxCommerce; user id = sa; password = p@ssw0rd";
+
+            var connection = new SqlConnection(connString);
+            connection.Open();
+
             var clientCheckpoint = new Checkpoint()
             {
                 TablesToIgnore = new[]
@@ -16,7 +21,7 @@ namespace DuxCommerce.Specifications.UseCases.Hooks
                 }
             };
 
-            clientCheckpoint.Reset(clientConnString);
+            clientCheckpoint.Reset(connection);
         }
     }
 }
