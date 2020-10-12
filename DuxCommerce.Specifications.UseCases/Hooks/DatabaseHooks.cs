e7600b71406a463e766514cefb90cb05f1c3fd24
Frank Wang <FrankWangSydney@gmail.com>
2020-10-12 14:54:29 +1100

    Removed unused AfterScenario Hook

--- DuxCommerce.Specifications.UseCases/Hooks/DatabaseHooks.cs
+++ DuxCommerce.Specifications.UseCases/Hooks/DatabaseHooks.cs
@@ -9,18 +9,10 @@ namespace DuxCommerce.Specifications.UseCases.Hooks
     [Binding]
     public sealed class DatabaseHooks
     {
-        // For additional details on SpecFlow hooks see http://go.specflow.org/doc-hooks
-
         [BeforeScenario]
         public void BeforeScenario()
         {
             DatabaseCleaner.CleanUp();
-        }
-
-        [AfterScenario]
-        public void AfterScenario()
-        {
-            //TODO: implement logic that has to run after executing each scenario
         }
     }
 }
