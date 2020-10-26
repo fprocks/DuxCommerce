a402c397c72e58d594c9a664fca929376d4c7e2c
Frank Wang <FrankWangSydney@gmail.com>
2020-10-26 13:59:24 +1100

    Call usecase in ShoppingCartController

--- DuxCommerce.Adapters.HttpControllers/Controllers/ShoppingCartController.fs
+++ DuxCommerce.Adapters.HttpControllers/Controllers/ShoppingCartController.fs
@@ -3,17 +3,17 @@
 open DuxCommerce.ShoppingCarts
 open Microsoft.AspNetCore.Mvc
 open Microsoft.Extensions.Logging
-open CatalogueComposition
+open ShoppingCartComposition
 
 [<ApiController>]
 [<Route("api/[controller]")>]
 type ShoppingCartController (logger : ILogger<ShoppingCartController>) =
     inherit ControllerBase()
 
-//    [<HttpPost("items")>]
-//    member this.Post(request: AddCartItemRequest) : IActionResult =
-//        let result = addCartItem request
-//        match result with
-//        | Ok p -> base.Ok(p) :> _
-//        | Error m -> base.BadRequest(m) :> _
-//  
+    [<HttpPost("items")>]
+    member this.Post(request: AddCartItemRequest) : IActionResult =
+        let result = addCartItem request
+        match result with
+        | Ok p -> base.Ok(p) :> _
+        | Error m -> base.BadRequest(m) :> _
+  
