1da499b06492e08c99051152e019dcee67ba0d1b
Frank Wang <FrankWangSydney@gmail.com>
2020-12-06 09:00:18 +1100

    Added relationships between shipping dtos

--- DuxCommerce.Core/Settings/Settings.PublicTypes.fs
+++ DuxCommerce.Core/Settings/Settings.PublicTypes.fs
@@ -2,78 +2,87 @@
 
 [<CLIMutable>]
 type AddressDto = {
-    Id : int64
-    FirstName : string
-    LastName : string
-    AddressLine1 : string
-    AddressLine2 : string
-    City : string
-    PostalCode : string
-    State : string
-    Country : string
+    Id: int64
+    FirstName: string
+    LastName: string
+    AddressLine1: string
+    AddressLine2: string
+    City: string
+    PostalCode: string
+    State: string
+    Country: string
 }
 
 [<CLIMutable>]
 type StoreDetailsDto = {
-    Id : int64
-    StoreName : string
-    ContactEmail : string
-    SenderEmail : string
-    BusinessName : string
+    Id: int64
+    StoreName: string
+    ContactEmail: string
+    SenderEmail: string
+    BusinessName: string
     PhoneNumber: string
-    TimeZoneId : string
-    UnitSystem : string
-    WeightUnit : string
-    LengthUnit : string
+    TimeZoneId: string
+    UnitSystem: string
+    WeightUnit: string
+    LengthUnit: string
     AddressId: int64
-    Address : AddressDto
+    Address: AddressDto
 }
 
 [<CLIMutable>]
 type LocationDto = {
-    Id : int64
-    Name : string
-    AddressId : int64
-    IsDefault : bool
+    Id: int64
+    Name: string
+    AddressId: int64
+    IsDefault: bool
+}
+
+[<CLIMutable>]
+type ShippingLocationDto = {
+    Id: int64
+    ShippingProfileId: int64
+    LocationId: int64
+}
+
+[<CLIMutable>]
+type ShippingStateDto = {
+    Id: int64
+    ShippingCountryId: int64
+    StateId: int64
+}
+
+[<CLIMutable>]
+type ShippingCountryDto = {
+    Id: int64
+    ShippingZoneId: int64
+    CountryId: int64
+    States: ShippingStateDto seq
+}
+
+[<CLIMutable>]
+type ShippingRatesDto = {
+    Id: int64
+    ShippingZoneId: int64
+    Name: string
+    ShippingType: string
+    Rate: decimal
+    Min: decimal
+    Max: decimal
+}
+
+[<CLIMutable>] 
+type ShippingZoneDto = {
+    Id: int64
+    Name: string
+    ShippingProfileId: int64
+    Rates: ShippingRatesDto seq
+    Countries: ShippingCountryDto seq
 }
 
 [<CLIMutable>]
 type ShippingProfileDto = {
     Id: int64
-    Name : string
+    Name: string
+    Locations: ShippingLocationDto seq
+    Zones: ShippingZoneDto seq
 }
-
-type ShippingLocationDto = {
-    Id : int64
-    ShippingProfileId : int64
-    LocationId : int64
-}
-
-type ShippingZoneDto = {
-    Id : int64
-    Name : string
-    ShippingProfileId : int64
-}
-
-type ShippingCountryDto = {
-    Id : int64
-    ShippingZoneId : int64
-    CountryId : int64
-}
-
-type ShippingStateDto = {
-    Id : int64
-    ShippingCountryId : int64
-    StateId : int64
-}
-
-type ShippingRatesDto = {
-    Id : int64
-    ShippingZoneId : int64
-    Name : string
-    ShippingType : string
-    Rate : decimal
-    Min : decimal
-    Max : decimal
-}
-
