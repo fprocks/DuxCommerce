e85c51a7a56fd254e1c7bbd3e2d802259b4cd141
fp Labs <FrankWangSydney@gmail.com>
2021-01-31 23:12:03 +1100

    Moved CustomerDto to Checkout.PublicTypes

---
+++ DuxCommerce.Core/Checkout/Checkout.PublicTypes.fs
@@ -0,0 +1,17 @@
+﻿namespace DuxCommerce.Core.Checkout.PublicTypes
+
+open MongoDB.Bson.Serialization.Attributes
+open MongoDB.Bson
+
+
+[<CLIMutable>]
+type CustomerDto = {
+    [<BsonId>]
+    [<BsonRepresentation(BsonType.ObjectId)>]
+    Id: string
+    Email: string
+    PhoneNumber: string
+    FirstName: string
+    LastName: string
+    UserId: string
+}
